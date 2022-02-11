d9643680c3c4368cd70832035cdd4717dcaa5b9c
juancarloscr17 <juankcr17@gmail.com>
2022-02-10 22:07:55 -0500

    Mostrar datos en la consola 05-02-2022

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+// document.getElementById("botonCerrar")
+
+//Abrir Ventana
+document.querySelector("#mostrarVentana").addEventListener("click", (e)=>{
+    document.querySelector("#ventana").show();
+})
+
+//Cerrar Ventana
+document.querySelector("#botonCerrar").addEventListener("click", (e)=>{
+    document.querySelector("#ventana").close();
+})
+
+
 
