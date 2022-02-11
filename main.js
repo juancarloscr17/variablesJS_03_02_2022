0442ff9b6a67a6e0e399107b32646c6fa283121a
juancarloscr17 <juankcr17@gmail.com>
2022-02-10 22:18:17 -0500

    Tipos de datos basados en objetos 10-02-2022

--- main.js
+++ main.js
@@ -1,9 +1,77 @@
-/*Tipo de dato null y undefined*/
-let variableExistente = null;
-let variableNoExistente = undefined;
+/******Tipo de dato Set***********/
+const data = new Set();
+data.add("Juan Carlos");
+data.add("Coronel");
 
-console.log(typeof(variableExistente));
-console.log(typeof(variableNoExistente));
+console.log(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*************Tipo d datos Maps************/
+// const lista = new Map();
+// lista.set("Nombre", "Juan");
+// lista.set("aPELLIDO", ["Coronel", "Rodriguez"]);
+// lista.delete("aPELLIDO");
+// console.log(lista);
+
+
+
+
+
+
+
+
+// 
+
+
+
+/*Tipo de dato Objeto*/
+// const informacion = {
+//     personal: {
+//         edad: 36,
+//         nombre: "Juan Carlos",
+//         altuta: 1.80
+//     },
+//     vivienda: {
+//         dirrecion: "Calle 35",
+//         pais: "Colombia",
+//         departamento: "Santander",
+//         lan: -7554.545,
+//         lon: 546.468
+//     },
+//     motrar(){
+//         return `${this.personal.nombre} * ${this.personal.edad}`;
+//     }
+// };
+// informacion.personal.nombre = "Juan Carlos Coronel";
+// console.log(informacion.motrar());
+// console.log(informacion);
+
+
+
+
+
+
+
+
+
+// /*Tipo de dato null y undefined*/
+// let variableExistente = null;
+// let variableNoExistente = undefined;
+
+// console.log(typeof(variableExistente));
+// console.log(typeof(variableNoExistente));
 
 
 
