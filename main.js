4ca68c5aa352f5197a1dbb55ab4b0a3a5a375759
juancarloscr17 <juankcr17@gmail.com>
2022-02-10 22:13:54 -0500

    Variables y tipos de datos  08-02-2022

--- main.js
+++ main.js
@@ -1,14 +1,129 @@
-// document.getElementById("botonCerrar")
+/*Tipo de dato null y undefined*/
+let variableExistente = null;
+let variableNoExistente = undefined;
 
-//Abrir Ventana
-document.querySelector("#mostrarVentana").addEventListener("click", (e)=>{
-    document.querySelector("#ventana").show();
-})
+console.log(typeof(variableExistente));
+console.log(typeof(variableNoExistente));
 
-//Cerrar Ventana
-document.querySelector("#botonCerrar").addEventListener("click", (e)=>{
-    document.querySelector("#ventana").close();
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Tipo de dato Bolean*/
+// let soySuProfesor = flase;
+// let soyUnEstudiante = true;
+// console.log(`Verdadero: ${soySuProfesor} Falso: ${soyUnEstudiante}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Tipo de datos Number*/
+// let edad = 36;
+// let altura = 1.80;
+
+// let moneda = Intl.NumberFormat("de-DE", {style: "currency", currency: "COP"}).format(1000);
+
+// console.log(`
+//     Edad : ${edad},
+//     Altura: ${altura},
+//     Moneda: ${moneda}
+// \n`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Tipo de datos String*/
+// let primerNombre = "Juan";
+// let segundoNombre = 'Carlos';
+// let apellidos = `Coronel Rodriguez`;
+
+// console.log(primerNombre, segundoNombre, apellidos);
+// console.log(primerNombre+' "'+"'"+toString(555).value+"\\n'"+'"');
+// console.log(segundoNombre.concat(apellidos));
+// console.log(`${primerNombre} "'${segundoNombre}'" `);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let nombre = `Juan Carlos Coronel Rodriguez`;
+
+/****Dato almacenado en el cache del navegador 'localStorage' ****/
+// localStorage.setItem("Dato", "Libro de cocina");
+// let recuperar = localStorage.getItem("Dato");
+// console.log(recuperar);
+
+
+/****** Constantes en JS ********/
+// const libros = 'Libro de cocina';
+// // libros = "Matematica";
+// console.log(libros);
+
+
+/*******Declaracion de tipo de datos heredado del objeto*****/
+// let numero = new Number(1000000);
+// let nombre = new String("Hola");
+// console.log(numero);
+
+
+
+
+
+
+
+
+
+
+
+// // document.getElementById("botonCerrar")
+
+// //Abrir Ventana
+// document.querySelector("#mostrarVentana").addEventListener("click", (e)=>{
+//     document.querySelector("#ventana").show();
+// })
+
+// //Cerrar Ventana
+// document.querySelector("#botonCerrar").addEventListener("click", (e)=>{
+//     document.querySelector("#ventana").close();
+// })
 
 
 
